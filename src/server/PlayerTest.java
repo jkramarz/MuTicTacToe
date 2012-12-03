c6c6a1666817bb4ae585e6a31cb58358280a96f2
Jakub Kramarz <lenwe@lenwe.net>
2012-12-03 11:02:13 +0100

    Player class unit tests

---
+++ src/server/PlayerTest.java
@@ -0,0 +1,31 @@
+package server;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.naming.directory.InvalidAttributesException;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void testPlayer() throws IOException, InvalidAttributesException {
+		Player p;
+		try{
+			p = new Player(null);
+		}catch(InvalidAttributesException e){
+			assertTrue(e.getMessage() == "Player socket must exists!");
+		}
+		
+		try{
+			Socket s = new Socket();
+			p = new Player(s);
+		}catch(InvalidAttributesException e){
+			assertTrue(e.getMessage() == "Player socket must be connected!");
+		}
+		
+	}
+}
