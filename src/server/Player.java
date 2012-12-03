f0d6de112b7e548776f59f40df037dc9dd61be69
Jakub Kramarz <lenwe@lenwe.net>
2012-12-03 11:01:45 +0100

    Invalid arguments check

--- src/server/Player.java
+++ src/server/Player.java
@@ -7,12 +7,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
+import javax.naming.directory.InvalidAttributesException;
+
 class Player {
 	BufferedReader reader;
 	BufferedWriter writer;
 	Socket socket;
 	
-	Player(Socket s) throws IOException{
+	Player(Socket s) throws IOException, InvalidAttributesException{
+	   if(s == null)
+		   throw new InvalidAttributesException("Player socket must exists!");
+	   if(s.isConnected() == false)
+		   throw new InvalidAttributesException("Player socket must be connected!");
 	   socket = s;
        reader = new BufferedReader(
     		   		new InputStreamReader(
@@ -26,7 +32,15 @@ class Player {
 				);
 	}
 	
-	Boolean isConnected(){
+	public BufferedReader getReader(){
+		return reader;
+	}
+	
+	public BufferedWriter getWriter(){
+		return writer;
+	}
+	
+	boolean isConnected(){
 		return socket.isConnected();
 	}
 	
